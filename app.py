75377d04f472eb0f479a0673b6e8757b4938f33e
João Paulo <161260658+Paulinzz@users.noreply.github.com>
2025-04-30 10:17:11 -0300

    esboço atividade 01

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# URL PATH ("/") | METHOD (GET,POST)
+@app.route('/', methods=['GET', 'POST'])
+def soma():
+    if request.method == 'POST':
+        n1 = int(request.form['n1'])
+        n2 = int(request.form['n2'])
+        resultado = n1 + n2
+        return render_template('index.html', resultado=resultado)
+    return render_template('index.html')
 
-#ROTA
-@app.route('/register', methods=['GET']) # a função register é chamada de "view function"
-def index(): # a função index é chamada de "view funciont"
-    #jinja2
-    return render_template('index.html') # renderiza o template index.html
-
-@app.route('/register', methods=['POST'])
-def cadastro():
-    nome = request.form['nome']
-    return f"voce digitou {nome}"
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
